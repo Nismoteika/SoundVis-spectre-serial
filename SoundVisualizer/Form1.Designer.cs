94530a51c6aa618d4488dda8743d137551f4cbb2
Павел Степанченко <stepanpahska@live.ru>
2019-07-14 18:53:55 +0300

    fft try

--- SoundVisualizer/Form1.Designer.cs
+++ SoundVisualizer/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.sel_device = new System.Windows.Forms.ComboBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // sel_device
@@ -38,16 +41,39 @@
             this.sel_device.Name = "sel_device";
             this.sel_device.Size = new System.Drawing.Size(328, 21);
             this.sel_device.TabIndex = 0;
+            this.sel_device.SelectedIndexChanged += new System.EventHandler(this.Sel_device_SelectedIndexChanged);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(40, 56);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.Btn_start_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(56, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 100);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 81);
+            this.ClientSize = new System.Drawing.Size(415, 254);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_start);
             this.Controls.Add(this.sel_device);
             this.Name = "Form1";
             this.Text = "VisibleSound";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +81,8 @@
         #endregion
 
         private System.Windows.Forms.ComboBox sel_device;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- SoundVisualizer/Form1.cs
+++ SoundVisualizer/Form1.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using NAudio.CoreAudioApi;
+using NAudio.Wave;
 
 namespace SoundVisualizer
 {
     public partial class Form1 : Form
     {
+        MMDevice device;
+        private IWaveIn waveIn;
+        private static int fftLength = 256;
+        private SampleAggregator sampleAggregator = new SampleAggregator(fftLength);
+        //Bitmap bitmap;
         public Form1()
         {
             InitializeComponent();
@@ -16,8 +23,74 @@ namespace SoundVisualizer
         private void Form1_Load(object sender, EventArgs e)
         {
             MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
-            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+            MMDeviceCollection devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
             sel_device.Items.AddRange(devices.ToArray());
+            //bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(OnDataAvailable), sender, e);
+            }
+            else
+            {
+                byte[] buffer = e.Buffer;
+                int bytesRecorded = e.BytesRecorded;
+                int bufferIncrement = waveIn.WaveFormat.BlockAlign;
+                for (int index = 0; index < bytesRecorded; index += bufferIncrement)
+                {
+                    float sample32 = BitConverter.ToSingle(buffer, index);
+
+                    sampleAggregator.Add(sample32);
+                }
+            }
+        }
+
+        private void FftCalculated(object sender, FftEventArgs e)
+        {
+            foreach(NAudio.Dsp.Complex C in e.Result)
+            {
+                Console.Write("X: " + C.X + "; Y: " + C.Y + " --- ");
+
+            }
+            Console.WriteLine();
+        }
+
+        private void Sel_device_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(sel_device.SelectedItem != null)
+                device = (MMDevice)sel_device.SelectedItem;
+        }
+
+        private void Btn_start_Click(object sender, EventArgs e)
+        {
+            sampleAggregator.FftCalculated += new EventHandler<FftEventArgs>(FftCalculated);
+            sampleAggregator.PerformFFT = true;
+
+            if (device != null)
+                waveIn = new WasapiLoopbackCapture(device);
+
+            waveIn.DataAvailable += OnDataAvailable;
+
+            waveIn.StartRecording();
+        }
+
+        public static float lerp(float a, float b, float t)
+        {
+            return a + (b - a) * t;
+        }
+
+        public static float map(float x, float x0, float x1, float a, float b)
+        {
+            float t = (x - x0) / (x1 - x0);
+            return lerp(a, b, t);
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            
         }
     }
 }
